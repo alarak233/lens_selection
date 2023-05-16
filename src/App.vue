acf29aaaff1d22d02542056bb5228639478319a3
alarak233 <727072895@qq.com>
2023-05-16 12:57:57 +0800

    初步界面

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,33 @@
     <h2>多点离焦镜片选型</h2>
     <p>联系方式:alarak2333@outlook.com</p>
   </div>
+  <div class="div_inline">
+    <div>
+      添加毛坯
+    </div>
+    <div>
+      添加环形微结构
+    </div>
+    <div>
+      添加多边形微结构
+    </div>
+    <div>
+      添加单个微结构
+    </div>
+  </div>
+  <div class="div_inline">
+    <h3>俯视图</h3>
+  </div>
+  <div class="div_inline">
+    <h3>剖视图</h3>
+  </div>
+  <div class="div_inline">
+    <h3>组件列表</h3>
+  </div>
 </template>
 
 <style scoped>
+.div_inline {
+  display: inline-block;
+}
 </style>
