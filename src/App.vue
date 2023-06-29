fd0b9b7a99ac35b2c02635564e372837db835b1f
alarak233 <727072895@qq.com>
2023-06-29 21:26:33 +0800

    补充环形微结构控制组件

--- src/App.vue
+++ src/App.vue
@@ -43,16 +43,16 @@ const water_marks_data1: Water_marks_data = {
   opacity: 0.1,
   color: "black",
   size: 25,
-  start_pos: {x:-400,y:20},
-  end_pos:{x:300,y:200}
+  start_pos: { x: -400, y: 20 },
+  end_pos: { x: 300, y: 200 }
 };
 const water_marks_data2: Water_marks_data = {
   text: "江苏工大金凯高端装备制造有限公司",
   opacity: 0.1,
   color: "black",
   size: 25,
-  start_pos: {x:300,y:20},
-  end_pos:{x:1000,y:200}
+  start_pos: { x: 300, y: 20 },
+  end_pos: { x: 1000, y: 200 }
 };
 </script>
 
@@ -62,52 +62,62 @@ const water_marks_data2: Water_marks_data = {
     <p>联系方式:alarak2333@outlook.com</p>
     <p>源码地址 <a href="https://github.com/alarak233/lens_selection">点击进入</a></p>
   </div>
-  <div class="div_inline">
-    <div>
-      添加毛坯
+  <div class="div_flex">
+    <div class="div_inline">
+      <div>
+        创建毛坯
+      </div>
+      <div>
+        添加等间距环形排布微结构
+      </div>
+      <div>
+        添加等间距多边形排布微结构
+      </div>
+      <div>
+        添加等间距直线排布微结构
+      </div>
+      <div>
+        添加单个微结构
+      </div>
     </div>
-    <div>
-      添加环形微结构
+    <div class="div_inline">
+      <h3>俯视图</h3>
+      <svg width="700" height="600">
+        <circle_array_cloud :circle_data="circle_data1" />
+        <line_array_cloud :line_data="line_data1" />
+        <polygon_array_cloud :polygon_data="polygon_data1" />
+        <water_marks :water_marks_data=water_marks_data1></water_marks>
+        <water_marks :water_marks_data=water_marks_data2></water_marks>
+      </svg>
     </div>
-    <div>
-      添加多边形微结构
+    <div class="div_inline">
+      <h3>剖视图</h3>
+      <h3>微结构示意图</h3>
     </div>
-    <div>
-      添加单个微结构
+    <div class="div_inline">
+      <h3>组件列表</h3>
+      <circle_array_control :circle_data="circle_data1" :id="1" />
+      <input type="number" v-model="line_data1.start_angle" />
+      <br>
+      <input type="number" v-model="polygon_data1.start_angle" />
+      <br>
+      <input type="number" v-model="polygon_data1.edge_num" />
+      <br>
+      <input type="number" v-model="polygon_data1.total_edge_num" />
+      <br>
+      <input type="number" v-model="polygon_data1.dist" />
+      <br>
+      <input type="number" v-model="circle_data1.start_r" />
     </div>
-  </div>
-  <div class="div_inline">
-    <h3>俯视图</h3>
-    <svg width="1100" height="900">
-      <circle_array_cloud :circle_data="circle_data1" />
-      <line_array_cloud :line_data="line_data1" />
-      <polygon_array_cloud :polygon_data="polygon_data1" />
-      <water_marks :water_marks_data=water_marks_data1></water_marks>
-      <water_marks :water_marks_data=water_marks_data2></water_marks>
-    </svg>
-  </div>
-  <div class="div_inline">
-    <h3>剖视图</h3>
-    <h3>微结构示意图</h3>
-  </div>
-  <div class="div_inline">
-    <h3>组件列表</h3>
-    <circle_array_control :circle_data="circle_data1" :id="1"/>
-    <input type="number" v-model="line_data1.start_angle" />
-    <br>
-    <input type="number" v-model="polygon_data1.start_angle" />
-    <br>
-    <input type="number" v-model="polygon_data1.edge_num" />
-    <br>
-    <input type="number" v-model="polygon_data1.total_edge_num" />
-    <br>
-    <input type="number" v-model="polygon_data1.dist" />
   </div>
 </template>
 
 <style scoped>
+.div_flex {
+  display: flex;
+}
+
 .div_inline {
-  display: inline-block;
   vertical-align: middle;
 }
 </style>
--- src/components/circle_array_control.vue
+++ src/components/circle_array_control.vue
@@ -4,7 +4,10 @@
         <ul>
             <li>微结构总圈数：{{ props.circle_data.points_num.length }}</li>
             <li>最内圈微结构环带半径：
-                <input type="text" v-model="props.circle_data.start_r"/>
+                <input type="number" v-model="props.circle_data.start_r" />
+            </li>
+            <li>间距：
+                <input type="number" v-model="props.circle_data.dist">
             </li>
             <li>每圈微结构数量
                 <button>add</button>
@@ -12,11 +15,23 @@
                     <li v-for="(item, index) in props.circle_data.points_num">
                         <label>数量
                             <input type="range" v-model="props.circle_data.points_num[index]" />
-                            <input type="number" v-model="props.circle_data.points_num[index]"  />
+                            <input type="number" v-model="props.circle_data.points_num[index]" />
                         </label>
                         <button>x</button>
                     </li>
                 </ol>
+            </li>
+        </ul>
+        <p>高级设置</p>
+        <ul>
+            <li>
+                <input type="checkbox" checked/>整圆
+            </li>
+            <li>圆弧角度
+                <input type="number" v-model="props.circle_data.end_angle"/>
+            </li>
+            <li>起始角度
+                <input type="number" v-model="props.circle_data.start_angle"/>
             </li>
         </ul>
     </div>
