8831cb6a7f701e9fa796bfc6cf91b3725870ae86
alarak233 <727072895@qq.com>
2023-07-20 21:31:13 +0800

    优化环形微结构控制组件

--- src/components/circle_array_control.vue
+++ src/components/circle_array_control.vue
@@ -1,8 +1,13 @@
 <template>
     <div>
-        <div>环形排布微结构{{ id }}</div>
+        <p>环形排布微结构{{ id }}
+            <button>v</button>
+        </p>
         <ul>
-            <li>微结构总圈数：{{ props.circle_data.points_num.length }}</li>
+            <li>微结构总圈数：{{ props.circle_data.points_num.length }}
+                <button>+</button>
+                <button>-</button>
+            </li>
             <li>最内圈微结构环带半径：
                 <input type="number" v-model="props.circle_data.start_r" />
             </li>
@@ -10,28 +15,32 @@
                 <input type="number" v-model="props.circle_data.dist">
             </li>
             <li>每圈微结构数量
-                <button>add</button>
                 <ol>
                     <li v-for="(item, index) in props.circle_data.points_num">
                         <label>数量
-                            <input type="range" v-model="props.circle_data.points_num[index]" />
+                            <input type="range" v-model="props.circle_data.points_num[index]" :max="200" />
                             <input type="number" v-model="props.circle_data.points_num[index]" />
                         </label>
-                        <button>x</button>
                     </li>
                 </ol>
             </li>
         </ul>
-        <p>高级设置</p>
+        <p>高级设置
+            <button>v</button>
+        </p>
         <ul>
+            <li v-if="circle_data.center">微结构中心
+                x:<input type="number" v-model="circle_data.center.x"/>
+                y:<input type="number" v-model="circle_data.center.y"/>
+            </li>
             <li>
-                <input type="checkbox" checked/>整圆
+                <input type="checkbox" checked />整圆
             </li>
             <li>圆弧角度
-                <input type="number" v-model="props.circle_data.end_angle"/>
+                <input type="number" v-model="props.circle_data.end_angle" />
             </li>
             <li>起始角度
-                <input type="number" v-model="props.circle_data.start_angle"/>
+                <input type="number" v-model="props.circle_data.start_angle" />
             </li>
         </ul>
     </div>
